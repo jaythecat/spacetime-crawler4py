f323cc667eec240fcb0b92b6da46601e390cfddd
Sascha Jordan Hynoski <shynoski@uci.edu>
2025-04-25 13:26:12 -0700

    is valid -> changed to true extract info not working for some reason

--- extractInfo.py
+++ extractInfo.py
@@ -1,5 +1,6 @@
 import json
 
+
 def tokenize(json_obj) -> dict[str, int]:
     num_unique = 0
     longest = {
@@ -11,7 +12,7 @@ def tokenize(json_obj) -> dict[str, int]:
     frequencies = {}
     for key, value in json_obj:
         if value != "":
-            num_unique+=1
+            num_unique += 1
         char_lower = value.lower()
         if char_lower.isalnum():
             curr.append(char_lower)
@@ -22,22 +23,24 @@ def tokenize(json_obj) -> dict[str, int]:
     if curr:
         tokens.append(''.join(curr))
     for token in tokens:
-
         frequencies[token] = frequencies.get(token, 0) + 1
     print("Unique Pages: " + num_unique)
     print(longest["url"] + ": " + str(longest["length"]))
     return frequencies
+
 
 def print_frequencies(frequencies: dict[str, int]) -> None:
     sorted_items = sorted(frequencies.items(), key=lambda item: (-item[1], item[0]))
     for token, count in sorted_items[:50]:
         print(f"{token} - {count}")
 
+
 def generate_report():
     with open("url_responses.json", 'r') as json_file:
         data = json.load(json_file)
         tokenized = tokenize(data)
         print_frequencies(tokenized)
+
 
 if __name__ == '__main__':
     generate_report()
