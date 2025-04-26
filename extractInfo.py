0e236426f7f57f72bbda6b1c15cf8e95b560af53
Sascha Jordan Hynoski <shynoski@uci.edu>
2025-04-25 18:16:05 -0700

    changed tokenize to work properly & refactored a bit -- still includes stop words

--- extractInfo.py
+++ extractInfo.py
@@ -1,51 +1,46 @@
 import json
+import re
 
 
 def tokenize(json_obj) -> dict[str, int]:
-    num_unique = 0
-    num_words = 0
+    frequencies = {}
+    filtered_words = ""
+    unique_pages = 0
     longest = {
         "length": -1,
         "url": ""
     }
-    curr = []
-    tokens = []
-    frequencies = {}
-    for key, value in json_obj:
-        if value != "":
-            num_unique += 1
-        char_lower = value.lower()
-        if char_lower.isalnum():
-            curr.append(char_lower)
-        else:
-            if curr:
-                tokens.append(''.join(curr))
-                curr = []
-                num_words += 1
-        if num_words > longest["length"]:
-            longest["length"] = num_words
-            longest["url"] = key
-    if curr:
-        tokens.append(''.join(curr))
+    for url, text in json_obj.items():
+        unique_pages += 1
+        if not text:
+            continue
+        words = text.lower().split()
+        filtered_words = [re.sub(r'[^a-z0-9]', '', word) for word in words]
+        filtered_words = [word for word in filtered_words if word.isalnum()]
 
-    for token in tokens:
+        if len(filtered_words) > longest["length"]:
+            longest["length"] = len(filtered_words)
+            longest["url"] = url
+
+    for token in filtered_words:
         frequencies[token] = frequencies.get(token, 0) + 1
-    print("Unique Pages: " + num_unique)
+    print(f"Unique Pages: {unique_pages}")
     print(longest["url"] + ": " + str(longest["length"]))
-    return frequencies
+    print_frequencies(frequencies)
 
 
 def print_frequencies(frequencies: dict[str, int]) -> None:
     sorted_items = sorted(frequencies.items(), key=lambda item: (-item[1], item[0]))
+    i = 1
     for token, count in sorted_items[:50]:
-        print(f"{token} - {count}")
+        print(f"{i}. {token} - {count}")
+        i += 1
 
 
 def generate_report():
     with open("url_responses.json", 'r') as json_file:
         data = json.load(json_file)
         tokenized = tokenize(data)
-        print_frequencies(tokenized)
 
 
 if __name__ == '__main__':
