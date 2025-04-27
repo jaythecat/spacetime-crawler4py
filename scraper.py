8f4715fa917236af0813c51a694f6c50c79e4e95
Johnna Louie <johnnalouie@gmail.com>
2025-04-27 11:22:55 -0700

    is_valid remove ical

--- scraper.py
+++ scraper.py
@@ -115,6 +115,10 @@ def is_valid(url):
         if re.search(r'/\d{4}-\d{2}(-\d{2})?', parsed.path):
             return False
 
+        # a url that is a copy for iCalendar - skip
+        if re.search(r'\?ical=1', url):
+            return False
+
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
