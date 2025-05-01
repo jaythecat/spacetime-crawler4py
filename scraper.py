eb54d15558641e57510a2b92400226ab5676865c
Johnna Louie <johnnalouie@gmail.com>
2025-04-30 19:24:10 -0700

    enforce scheme

--- scraper.py
+++ scraper.py
@@ -91,10 +91,9 @@ def is_valid(url):
     try:
         parsed = urlparse(url)
 
-        """
         if parsed.scheme not in set(["http", "https"]):
             return False 
-        """
+
 
         """
         Check if url has already been scraped
